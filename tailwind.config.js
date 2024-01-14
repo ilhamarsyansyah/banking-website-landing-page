a02e3edcc65324113028bf20ae72a4d56fabf9bf
ilhamarsyansyah <arsyansyah86@gmail.com>
2024-01-14 14:03:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        "pink": '#ED017F',
+        "abu": '#999999',
+        "hitam": '#343434',
+        "abu-tua": '#676767'
+      }
+    },
+  },
+  plugins: [],
+}
